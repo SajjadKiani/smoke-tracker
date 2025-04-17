05ccbc8a6d384cf7043a2cbef6929a6f7bcec2cd
0x000Saji <skm.kiani@gmail.com>
2025-04-17 15:05:04 +0330

    fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -4,5 +4,5 @@ export default withPWA({
   dest: "public",
   runtimeCaching: [],          // filled in gist for brevity
   disable: process.env.NODE_ENV === "development",
-  experimental: { appDir: true },
+  experimental: 'exclude'
 });
