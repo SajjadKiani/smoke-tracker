4d6a84c4fa7467c7a40c6ca18e7c3d18406f5f6e
0x000Saji <skm.kiani@gmail.com>
2025-04-17 17:23:32 +0330

    add image optimizer

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,15 @@ import withPWA from "next-pwa";
 export default withPWA({
   dest: "public",
   runtimeCaching: [],          // filled in gist for brevity
-  disable: process.env.NODE_ENV === "development"
+  disable: process.env.NODE_ENV === "development",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 });
