a559e92f1c917a42cf24bf610a7a669ad99954b9
0x000Saji <skm.kiani@gmail.com>
2025-04-17 15:07:26 +0330

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,5 @@ import withPWA from "next-pwa";
 export default withPWA({
   dest: "public",
   runtimeCaching: [],          // filled in gist for brevity
-  disable: process.env.NODE_ENV === "development",
-  experimental: 'exclude'
+  disable: process.env.NODE_ENV === "development"
 });
